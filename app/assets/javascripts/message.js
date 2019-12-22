1eaba122959643fb449de152fc31a0984ae875ca
ogaryou <57928285+ogaryou@users.noreply.github.com>
2019-12-22 23:09:00 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,6 @@
 $(function(){ 
-  
   function buildHTML(message){
-   if ( message.image ) {
+   if ( message.image && message.image) {
      var html =
       `<div class="main-chat__message" data-message-id=${message.id}>
          <div class="main-chat__message__box">
@@ -21,7 +20,7 @@ $(function(){
            </p>
          </div>
        </div>`
-   } else {
+   } else if (message.image){
      var html =
       `<div class="main-chat__message" data-message-id=${message.id}>
          <div class="main-chat__message__box">
@@ -38,9 +37,27 @@ $(function(){
            </p>
          </div>
        </div>`
+   } else if (message.content) {
+    var html =
+    `<div class="main-chat__message" data-message-id=${message.id}>
+       <div class="main-chat__message__box">
+         <div class="main-chat__message__box__name">
+           ${message.user_name}
+         </div>
+         <div class="main-chat__message__box__data">
+           ${message.created_at}
+         </div>
+       </div>
+       <div class="main-chat__message__text">
+         <p class="lower-message__content">
+           ${message.content}
+         </p>
+       </div>
+     </div>`
+   };
      return html;
    };
- }
+ 
  $('#new_message').on('submit', function(e){
    e.preventDefault();
    var formData = new FormData(this);
@@ -80,8 +97,8 @@ $(function(){
         messages.forEach(function (message) {
         insertHTML = buildHTML(message);
       })
-      $('.message').append(insertHTML);
-      $('.message').animate({ scrollTop: $('.message')[0].scrollHeight}, 'fast');
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
       .fail(function() {
         alert('自動更新に失敗しました');
