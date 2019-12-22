8e37f94cda9c9cddfa84dffb5b7ef75c041ff2d3
ogaryou <57928285+ogaryou@users.noreply.github.com>
2019-12-22 21:43:33 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){ 
   function buildHTML(message){
-   if ( message.image ) {
+   if ( message.image && message.content ) {
      var html =
       `<div class="main-chat__message" data-message-id=${message.id}>
          <div class="main-chat__message__box">
@@ -20,7 +20,7 @@ $(function(){
            </p>
          </div>
        </div>`
-   } else {
+   } else if (message.content) {
      var html =
       `<div class="main-chat__message" data-message-id=${message.id}>
          <div class="main-chat__message__box">
@@ -37,6 +37,24 @@ $(function(){
            </p>
          </div>
        </div>`
+   } else if (message.image) {
+    var html =
+    `<div class="main-chat__message" data-message-id=${message.id}>
+       <div class="main-chat__message__box">
+         <div class="main-chat__message__box__name">
+           ${message.user_name}
+         </div>
+         <div class="main-chat__message__box__data">
+           ${message.created_at}
+         </div>
+       </div>
+       <div class="main-chat__message__text">
+         <p class="lower-message__content">
+           ${message.content}
+         </p>
+       </div>
+     </div>`
+   };   
      return html;
    };
  }
@@ -53,6 +71,7 @@ $(function(){
      contentType: false
    })
     .done(function(data){
+      console.table(data)
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
@@ -75,13 +94,12 @@ $(function(){
         data: {last_id: last_message_id},
       })
       .done(function(messages) {
-        console.log(messages)
         var insertHTML = '';
-        messages.forEach(function (message) {
-        insertHTML = buildHTML(message);
+        messages.forEach(function (message,i) {
+        insertHTML += buildHTML(message);
       })
-      $('.message').append(insertHTML);
-      $('.message').animate({ scrollTop: $('.message')[0].scrollHeight}, 'fast');
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
       .fail(function() {
         alert('自動更新に失敗しました');
